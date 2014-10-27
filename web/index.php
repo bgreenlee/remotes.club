b6d86e169de7195fe2004205a60f3ddb90fa7959
Brad Greenlee <brad@footle.org>
2014-10-27 08:29:56 -0700

    Mark members who have updated their home page in the last 24 hours

--- web/index.php
+++ web/index.php
@@ -1,6 +1,9 @@
 <html>
 <head>
 <title>remotes.club</title>
+<style type="text/css">
+ul.members { list-style-type: none; }
+</style>
 </head>
 <body>
 <h1>remotes.club</h1>
@@ -12,17 +15,35 @@
 <p>If you'd like a remotes.club account, send an email to <a href="mailto:hi@remotes.club">hi@remotes.club</a> introducing yourself. Include your desired username and your <a href="https://help.github.com/articles/generating-ssh-keys/">public ssh key</a>.</p>
 
 <h2>Members</h2>
-<ul>
+<ul class="members">
 <?php
 $homedirs = array_filter(scandir("/home"), function($dir) {
   return !preg_match("/^\.|www/", $dir) && is_dir("/home/$dir");
 });
 
+$index_files = ["index.html", "index.htm", "index.php"];
 foreach ($homedirs as $homedir) {
-  print("<li><a href=\"https://$homedir.remotes.club\">$homedir</a></li>\n");
+  $last_update = array_reduce($index_files, function($carry, $index_file) use ($homedir) {
+    $path = "/home/$homedir/web/$index_file";
+    if (file_exists($path)) {
+      $mtime = filemtime($path);
+      if ($mtime > $carry) {
+        return $mtime;
+      } else {
+        return $carry;
+      }
+    } else {
+      return $carry;
+    }
+  }, 0);
+
+  $updated_marker = $last_update > time() - 86400 ? "*" : "";
+
+  print("<li data-last-update=\"$last_update\"><span><a href=\"https://$homedir.remotes.club\">$homedir</a></span> $updated_marker</li>\n");
 }
 ?>
 </ul>
+<p><small>* Updated in the last 24 hours</small></p>
 
 </body>
 </html>
